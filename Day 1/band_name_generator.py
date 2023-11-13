cad871a82f21a870201124caea1b25227f184744
Kaiwalya Deshpande <kaiwalya@live.in>
2023-11-14 01:57:29 +0530

    Day - 1

---
+++ Day 1/band_name_generator.py
@@ -0,0 +1,8 @@
+"""
+## Band Name Generator
+It generates a new name for your band
+"""
+print("Welcome the the Band Name Generator!!!")
+city = input("Which city did you grow up in?\n")
+pet = input("What is the name of your pet?\n")
+print("The name of your band should be : "+city+" "+pet)
---
+++ Day 1/main.py
@@ -0,0 +1,11 @@
+print("Hello World\nHello World\nHello World")
+print("Hello"+" Kaiwalya")
+print("Hello "+input("What is your name? :"))
+# This is a comment
+name = "Jack"
+print(name)
+name = "Angela"
+print(name)
+name = input("What is your name : ")
+length = len(name)
+print(length)
