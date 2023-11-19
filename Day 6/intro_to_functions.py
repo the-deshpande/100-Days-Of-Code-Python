589d3b71bc164a4d961c12c00bcea4f6beaa94e7
Kaiwalya Deshpande <kaiwalya@live.in>
2023-11-19 10:50:41 +0530

    Day - 6

---
+++ Day 6/intro_to_functions.py
@@ -0,0 +1,11 @@
+# Pre-Defined Function
+print("Hello")
+num_char = len('Hello')
+print(num_char)
+
+# Custom functions
+def my_function(): # Defining the function
+    print("Hello")
+    print("Bye")
+
+my_function() # Calling the function
