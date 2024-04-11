0f783709222bbe0b00a2672ab9aa23f3f853267b
Kaiwalya Deshpande <kaiwalya@live.in>
2024-04-11 23:27:26 +0530

    Day 83

---
+++ Day 83/index.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap5
+
+app = Flask(__name__)
+Bootstrap5(app)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ Day 83/readme.md
@@ -0,0 +1,5 @@
+## Portfolio
+
+- This is a portfolio type website using python and flask
+- All the css has been done using Bootstrap
+- Alot of the website design have been inspired from the bootstrap provided templates
